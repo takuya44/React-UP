9fa2a82f97cb9bfa624f66c468ce2f925786a5b3
takuya44 <78689643+takuya44@users.noreply.github.com>
2021-03-14 12:00:40 +0900

    css 当て方　学習中

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import { useState, useCallback } from "react";
-import { ChildArea } from "./ChildArea";
+import { CssModules } from "./components/CssModules";
+import { InlineStyle } from "./components/InlineStyle";
 import "./styles.css";
 
 export default function App() {
-  // console.log("App");
-  const [text, setText] = useState("");
-  const [open, setOpen] = useState(false);
-
-  const onChangeText = (e) => setText(e.target.value);
-
-  const onClickOpen = () => setOpen(!open);
-
-  const onClickClose = useCallback(() => setOpen(false), []);
-
   return (
     <div className="App">
-      <input value={text} onChange={onChangeText} />
-      <br />
-      <br />
-      <button onClick={onClickOpen}>表示</button>
-      <ChildArea open={open} onClickClose={onClickClose} />
+      <InlineStyle />
+      <CssModules />
     </div>
   );
 }
