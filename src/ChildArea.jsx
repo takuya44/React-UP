d55d225165f998da4d64f0a94a3d3806dc42bce1
takuya44 <78689643+takuya44@users.noreply.github.com>
2021-03-14 11:10:01 +0900

    s s

--- src/ChildArea.jsx
+++ src/ChildArea.jsx
@@ -1,18 +1,26 @@
+import { memo } from "react"; //memoすご
+
 const style = {
   width: "100%",
   height: "200px",
   background: "khaki"
 };
 
-export const ChildArea = (props) => {
-  const { open } = props;
+export const ChildArea = memo((props) => {
+  const { open, onClickClose } = props;
+
+  const data = [...Array(2000).keys()];
+  data.forEach(() => {
+    console.log("...");
+  });
   return (
     <>
       {open ? (
         <div style={style}>
           <p>子コンポーネント</p>
+          <button onClick={onClickClose}>閉じる</button>
         </div>
       ) : null}
     </>
   );
-};
+});
