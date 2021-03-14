60a4524a94a3d7b4c2923e3b7c77927f79deea94
takuya44 <78689643+takuya44@users.noreply.github.com>
2021-03-14 12:44:38 +0900

    css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import { CssModules } from "./components/CssModules";
 import { InlineStyle } from "./components/InlineStyle";
+import { StyledComponents } from "./components/StyledCoponents";
+import { StyledJsx } from "./components/StyledJsx";
+import { Emotion } from "./components/Emotion";
+
 import "./styles.css";
 
 export default function App() {
@@ -7,6 +11,9 @@ export default function App() {
     <div className="App">
       <InlineStyle />
       <CssModules />
+      <StyledJsx />
+      <StyledComponents />
+      <Emotion />
     </div>
   );
 }
